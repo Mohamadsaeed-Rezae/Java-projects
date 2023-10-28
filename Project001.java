7e44836029bcee874f402d01f48c02959fa13870
Mohamadsaeed <138451483+Mohamadsaeed-Rezae@users.noreply.github.com>
2023-10-28 19:30:34 +0330

    Add files via upload

---
+++ Project001.java
@@ -0,0 +1,24 @@
+
+public class Project001 {
+
+
+    public static void main(String[] args) {
+        int n =12345;
+        System.out.format("the number is %d\n",n);
+        System.out.format("the number is %08d\n",n);
+        System.out.format("the number is %+8d\n",n);
+        System.out.format("the number is %,d\n",n);
+        System.out.format("the number is %,8d\n",n);
+        System.out.format("the number is %-8d\n",n);
+        
+        double pi=Math.PI;
+        System.out.format("pi=%f\n", pi);
+        System.out.format("pi=%.2f\n", pi);
+        System.out.format("pi=%-8.8f\n", pi);
+
+
+
+                
+    }
+    
+}
