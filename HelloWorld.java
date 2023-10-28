20e348df830721801eb45fdec9611eab820902b3
Mohamadsaeed <138451483+Mohamadsaeed-Rezae@users.noreply.github.com>
2023-10-28 20:07:30 +0330

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,14 @@
-public class HelloWorld{
-    public static void main(String[]args){
-        System.out.println("Hello world");
-        System.out.println("This is my first java program");
+import java.util.*;
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.print("hello world\n");
+        System.out.print("Hi java!\n");
+        
+        System.out.println("\n press enter to continue...");
+        Scanner input=new Scanner(System.in);
+        input.nextLine();
     }
+    
 }
