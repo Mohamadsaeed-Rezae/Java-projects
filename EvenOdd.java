a79624162913193e8998e63104d9cdadc0ebf322
Mohamadsaeed <138451483+Mohamadsaeed-Rezae@users.noreply.github.com>
2023-11-08 19:55:44 +0330

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class EvenOdd {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("Enter a number:");
+        Scanner input= new Scanner(System.in);
+        int n = input.nextInt();
+        if (n % 2==0){
+          System.out.format("%d is even\n",n);
+        }
+        if (n % 2==1){
+          System.out.format("%d is odd\n",n);
+        } 
+        
+        
+        
+        
+        
+   
+        }
+            
+            
+       
+    }
+    
+}
