504684d172567f0d6070124fe0e7548622e8ce0f
Mohamadsaeed <138451483+Mohamadsaeed-Rezae@users.noreply.github.com>
2023-11-08 19:56:34 +0330

    Add files via upload

---
+++ Project004.java
@@ -0,0 +1,14 @@
+
+public class Project004 {
+    
+    public static void main(String[] args) {
+       int a , b ,c ;
+       a =10;
+       b=a++;
+       c=++a;
+       System.out.format("%d\n",a);
+       System.out.format("%d\n",b);
+       System.out.format("%d\n",c);
+    }
+    
+}
