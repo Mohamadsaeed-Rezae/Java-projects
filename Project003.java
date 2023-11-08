d0c509228a0d760a59638289720152db479bea2d
Mohamadsaeed <138451483+Mohamadsaeed-Rezae@users.noreply.github.com>
2023-11-08 19:56:15 +0330

    Add files via upload

---
+++ Project003.java
@@ -0,0 +1,21 @@
+import  java.util.*;
+public class Project003 {
+
+    public static void main(String[] args) {
+      System.out.println("enter a number");
+      Scanner input=new Scanner(System.in);
+      int n = input.nextInt();
+   
+      
+      if(n ==6){
+      System.out.println("you have entered 6");
+      }
+      
+      if(n ==3){
+      System.out.println("you have entered 3");
+      }
+      }
+      
+    }
+    
+}
